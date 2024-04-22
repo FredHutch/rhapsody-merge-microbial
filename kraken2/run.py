28bc7866b59fdfc5f258162877e78e3def49e223
Sam Minot <sminot@fredhutch.org>
2024-04-22 11:22:49 -0700

    Parse cell barcodes from BAM file

--- kraken2/run.py
+++ kraken2/run.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import pandas as pd
 import gzip
 import click
+import pysam
 
 
 def parse_fastq(fastq):
@@ -19,36 +20,65 @@ def parse_fastq(fastq):
                 pass
 
 
-def map_tax_to_cell(tax_long: pd.DataFrame, fastq_1, fastq_2):
+def map_tax_to_cell(tax_long: pd.DataFrame, bam: str):
 
-    # Build a table of taxonomic classification results
-    output = defaultdict(lambda: defaultdict(int))
+    # Make a lookup set with the read IDs which have been classified
+    classified_reads = set(tax_long.index)
 
-    # Iterate over both FASTQ files in parallel
-    for (header1, seq1), (header2, seq2) in zip(
-        parse_fastq(fastq_1),
-        parse_fastq(fastq_2)
-    ):
-        # If there is no taxonomic classification result, skip it
-        if tax_long.get(header1) is None:
-            continue
+    print(f"Searching for cell barcodes for {len(classified_reads):,} reads in {bam}")
 
-        # Otherwise, get the cell barcode
-        cell_barcode = get_cell_barcode(header1, seq1, header2, seq2)
+    # Make a dict with the cell barcode for each read ID
+    cb = {}
 
-        # Add the taxonomic classification result to the table
-        output[cell_barcode][tax_long[header1]] += 1
+    # Open the BAM file and iterate over every line
+    with pysam.AlignmentFile(bam, "rb") as f:
+        for read in f:
+            # Skip reads which have not been classified
+            if read.query_name not in classified_reads:
+                continue
 
-    # Convert the table to a wide format
-    tax_wide = pd.DataFrame(output).fillna(0)
-    # report the number of reads classified to each taxon for each cell barcode
-    print(f"Taxonomic classification results:\n{tax_wide.shape[0]:,} rows x {tax_wide.shape[1]:,} columns")
-    return tax_wide
+            # Extract the cell barcode from the read
+            cell_barcode = get_cell_barcode(read)
+
+            # Add the cell barcode to the taxonomic classification table
+            cb[read.query_name, "cell_barcode"] = cell_barcode
+
+    cb = pd.Series(cb)
+
+    print(f"Found cell barcodes for {cb.shape[0]:,} reads")
 
 
-def get_cell_barcode(header1, seq1, header2, seq2):
+def get_cell_barcode(read: pysam.AlignedSegment):
     """Get the cell barcode for a given pair of reads."""
-    return "BARCODE"  # FIXME
+    return read.get_tag("CB")
+
+
+def get_tax_long(kraken_output):
+    tax_long = pd.concat([
+        (
+            df
+            .query("flag == 'C'")
+            .query("tax_id != 1")
+            .query("tax_id != 2")
+            .query("tax_id != 9606")
+            .query("tax_id != 28384")
+            .query("tax_id != 131567")
+        )
+        for df in pd.read_csv(
+            kraken_output,
+            sep="\t",
+            header=None,
+            names=["flag", "read_id", "tax_id"],
+            usecols=[0, 1, 2],
+            iterator=True,
+            chunksize=10000
+        )
+    ]).set_index("read_id")["tax_id"].to_dict()
+    print(f"Taxonomic classification results for {len(tax_long):,} reads")
+    print("Top hits:")
+    print(pd.Series(tax_long).value_counts().head(10))
+
+    return tax_long
 
 
 @click.command()
@@ -62,34 +92,12 @@ def main(kraken_output, barcodes_tsv, bam):
     print(f"Single cell table file: {barcodes_tsv}")
     print(f"BAM file: {bam}")
 
-    # # Read in the table of taxonomic classification results
-    # # Transform to a dict with read_id as key and tax_id as value
-    # tax_long = pd.concat([
-    #     (
-    #         df
-    #         .query("flag == 'C'")
-    #         .query("tax_id != 1")
-    #         .query("tax_id != 2")
-    #         .query("tax_id != 9606")
-    #         .query("tax_id != 28384")
-    #         .query("tax_id != 131567")
-    #     )
-    #     for df in pd.read_csv(
-    #         kraken_output,
-    #         sep="\t",
-    #         header=None,
-    #         names=["flag", "read_id", "tax_id"],
-    #         usecols=[0, 1, 2],
-    #         iterator=True,
-    #         chunksize=10000
-    #     )
-    # ]).set_index("read_id")["tax_id"].to_dict()
-    # print(f"Taxonomic classification results for {len(tax_long):,} reads")
-    # print("Top hits:")
-    # print(pd.Series(tax_long).value_counts().head(10))
+    # Read in the table of taxonomic classification results
+    # Transform to a dict with read_id as key and tax_id as value
+    tax_long = get_tax_long(kraken_output)
 
-    # # Map each taxonomic classification result to the cell barcode
-    # tax_wide = map_tax_to_cell(tax_long, fastq_1, fastq_2)
+    # Map each read ID to the cell barcode
+    tax_wide = map_tax_to_cell(tax_long, bam)
 
     # # Merge the table with the single cell table
     # # FIXME
